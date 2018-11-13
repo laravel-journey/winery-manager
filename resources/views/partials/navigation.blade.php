9c60d76e755ca15c8e8c495c2a5543d94072ddc0
Francesco Malatesta <hellofrancesco@gmail.com>
2018-11-13 22:22:23 +0100

    Split in separate views

---
+++ resources/views/partials/navigation.blade.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+    <div class="container">
+        <a class="navbar-brand" href="#">Winery Manager</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Menu Item</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
