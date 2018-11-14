f779bf187498a1aee180add8d1afc9d068e0fe64
Francesco Malatesta <hellofrancesco@gmail.com>
2018-11-14 22:13:01 +0100

    Add migration for producers table

---
+++ database/migrations/2018_11_14_203755_create_producers_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProducersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('producers', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('name');
+            $table->string('website');
+
+            $table->timestamps();
+        });
+
+        Schema::table('wines', function(Blueprint $table) {
+            $table->integer('producer_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('wines', function(Blueprint $table){
+            $table->dropColumn('producer_id');
+        });
+
+        Schema::dropIfExists('producers');
+    }
+}
